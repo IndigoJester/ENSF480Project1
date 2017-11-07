6ba18a01de1d09f319f3ada9fde4df4c92b768ca
Mitchell <mitchfnewell@gmail.com>
2017-11-07 11:36:38 -0700

    added products.java
    added products.java

---
+++ Product.java
@@ -0,0 +1,46 @@
+import java.util.Date;
+
+public class Product {
+	private String name;
+	private Date created;
+	private int numberOfBugs;
+	private String details;
+	
+	public Product(String name, Date created, int numberOfBugs, String details) {
+		this.name = name;
+		this.created = created;
+		this.numberOfBugs = numberOfBugs;
+		this.details = details;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void serCreated(Date created) {
+		this.created = created;
+	}
+	
+	public Date getCreated() {
+		return created;
+	}
+	
+	public void setNumberOfBugs(int numberOfBugs) {
+		this.numberOfBugs = numberOfBugs;
+	}
+	
+	public int getNumberOfBugs() {
+		return numberOfBugs;
+	}
+	
+	public void setDetails( String details) {
+		this.details = details;
+	}
+	
+	public String getDetails() {
+		return details;
+	}
+}
