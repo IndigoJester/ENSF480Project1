453126dfce174641fff7f5b81dd7c6a8494a37ab
DavisRomn <davisabr@gmail.com>
2017-11-16 13:05:22 -0700

    Added AssignBugWindow.java

---
+++ AssignBugWindow.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.io.*;
+
+public class AssignBugWindow {
+    private Developer[] developers;
+    private JList developerList;
+    private JButton assign, cancel;
+    private EventListener buttonEventListener;
+
+    public AssignBugWindow() {
+        this.display();
+    }
+
+    public void display() {
+        // display
+    }
+
+    private void assign() {
+        //
+    }
+
+    private void cancel() {
+        // 
+    }
+}
