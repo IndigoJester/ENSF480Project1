4a6e7ef7cfd56eea00a9a65801d8e7fed18f19eb
DavisRomn <davisabr@gmail.com>
2017-11-16 13:19:23 -0700

    Added BugUpdateWindow.java

---
+++ BugUpdateWindow.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.io.*;
+
+public class BugUpdateWindow {
+    private Bug theBug;
+    private JTextField statusUpdate;
+    private JButton submitUpdate, cancelUpdate;
+    private EventListener buttonEventListener;
+    public JCheckBox fixed;
+
+    public BugUpdateWindow() {
+        this.display();
+    }
+
+    public void display() {
+        // Display.
+    }
+
+    private void cancel() {
+
+    }
+
+    private void submit() {
+
+    }
+}
