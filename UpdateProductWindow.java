05103bad44c06cafe873872c0b0a7653961c9d7e
DavisRomn <davisabr@gmail.com>
2017-11-16 13:28:25 -0700

    Added UpdateProductWindow.java

---
+++ UpdateProductWindow.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.io.*;
+import java.util.EventListener;
+
+public class UpdateProductWindow {
+    private Product theProduct;
+    private JTextField productStatus;
+    private JButton submitUpdate, cancelUpdate;
+    private EventListener buttonEventListener;
+
+    public UpdateProductWindow() {
+        this.display();
+    }
+
+    public void display() {
+        // Display.
+    }
+
+    private void cancel() {
+        //
+    }
+
+    private void submit() {
+        // 
+    }
+}
