4015d3233e83c5574811d6576072a3db142bbf69
Jared Brintnell <jdbrintnell@outlook.com>
2017-11-16 13:13:48 -0700

    custom GUI elements

--- LoginWindow.java
+++ LoginWindow.java
@@ -25,7 +25,7 @@ public class LoginWindow {
     private void display () {
         // Set up display defaults
         Font defaultFont = new Font("Candara", Font.BOLD, 20);
-        Color defaultColor = new Color (36, 33, 33);
+        Color defaultColor = new Color (54, 51, 51);
 
         // Create Frame
         JFrame main = new JFrame();
@@ -40,20 +40,42 @@ public class LoginWindow {
         // Set background image
         BackgroundPanel background = new BackgroundPanel(new BorderLayout(), "Images/Login.png");
 
-        //JPanel content
+        //JPanel setup
         JPanel content = new JPanel();
         content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
         content.setOpaque(false);
-        content.setBorder(new EmptyBorder (0, 50, 0, 0));
+        content.setBorder(new EmptyBorder (0, 50, 0, 50));
+
+        // Username stuff
         JLabel label = new JLabel("Username:");
+        label.setBorder(new EmptyBorder (12, 0, 12, 0));
 		label.setForeground(defaultColor);
 		label.setFont(defaultFont);
 		content.add(label);
+        username = new CustomJTextField(60);
+        content.add(username);
 
+        // Password stuff
+        label = new JLabel("Password:");
+        label.setBorder(new EmptyBorder (12, 0, 12, 0));
+		label.setForeground(defaultColor);
+		label.setFont(defaultFont);
+		content.add(label);
+        password = new CustomJPasswordField(60);
+        content.add(password);
+
+        // Buttons
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout (FlowLayout.CENTER, 46, 0));
+        buttonPanel.setOpaque(false);
+        buttonPanel.setBorder(new EmptyBorder (20, 0, 20, 0));
+        loginButton = new JButton("Login");
+        buttonPanel.add(loginButton);
+        defaultButton = new JButton("Default User");
+        buttonPanel.add(defaultButton);
+
+        content.add(buttonPanel);
         background.add("South", content);
-
-
-
         main.add(background);
         main.setVisible(true);
     }
