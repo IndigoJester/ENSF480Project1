8f0148f2f002864eb2c36b9ab89b4c940479934c
DavisRomn <davisabr@gmail.com>
2017-11-16 13:31:25 -0700

    Added AddProductWindow.java

---
+++ AddProductWindow.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.io.*;
+
+public class AddProductWindow {
+    private JTextField name, details;
+    private JButton submit, cancel;
+
+    public AddProductWindow() {
+        this.display();
+    }
+
+    public void display() {
+        // Display.
+    }
+
+    private void addNewProduct() {
+        // Add a new Product.
+    }
+
+    private void cancelNewProduct() {
+        // Cancel a new Product.
+    }
+}
