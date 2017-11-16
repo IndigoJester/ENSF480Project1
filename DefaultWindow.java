58f8d93e63bddbf7b5a4a1cf0e4fe7e972e8e524
DavisRomn <davisabr@gmail.com>
2017-11-16 12:44:21 -0700

    Added Default Window Class with base functions and variables.

---
+++ DefaultWindow.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.io.*;
+
+class DefaultWindow {
+    protected JList productList, bugList;
+    protected Bug[] bugs;
+    protected JButton submitBug, refresh;
+    protected MouseAdapter mouseEventListener;
+    protected EventListener buttonEventListener;
+    protected Product activeProduct;
+
+    public void display() {
+        // Add Display Code Here.
+    }
+
+    protected void getBugs() {
+        // Get Bugs from the Database
+    }
+
+    protected void getProjects() {
+        // Get Products from the Database
+    }
+
+    protected void viewBug(Bug theBug) {
+        // View the Bug.
+    }
+
+    protected void viewProduct(Product theProduct) {
+        // View the Product
+    }
+
+    protected void showBugsFrom(Product activeProduct) {
+        // Display thec bugs from the product argument
+    }
+
+    protected void submitNewBug(Product activeProduct) {
+        // Display the Bugs from the product argument
+    }
+
+    protected void refresh() {
+        // Refresh.
+    }
+}
