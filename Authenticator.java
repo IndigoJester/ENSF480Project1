4588bed731e7f131107273e44e4f4af31b6f267e
DavisRomn <davisabr@gmail.com>
2017-11-17 15:12:12 -0700

    Added Comments to Authenticator

--- Authenticator.java
+++ Authenticator.java
@@ -4,26 +4,33 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+// Authenticator Class - Used to authenticate user credentials
 public class Authenticator {
+    //Connection to the SQL database
 	private static Connection myConn;
-	
+
+    // Default Constructor, sets up connection to Database
 	public Authenticator() throws SQLException {
-		
+
 		myConn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/credentials?autoReconnect=true&useSSL=false", "root", "Bigpapi");
-		
+
 	}
-	
+
+    // Generates the Developer Window
 	public static void generateDevWindow(String username) throws SQLException{
 		DeveloperWindow devWindow = new DeveloperWindow(username);
-		
+
 	}
+
+    //Generates the Managed Window
 	public static void generateManagerWindow(String username) throws SQLException {
-		
+
 		 ProjectManagerWindow managerWindow = new ProjectManagerWindow(username);
 	}
-	
+
+    // Boolean Function that returns true if credentials are found to be correct, otherwise returns false
 	public static boolean authenticate(String username , String password) throws SQLException {
-		
+
 		PreparedStatement stmt = myConn.prepareStatement("SELECT * FROM credentials WHERE username = ?");
 		stmt.setString(1, username);
 		ResultSet rs = stmt.executeQuery();
@@ -36,7 +43,7 @@ public class Authenticator {
 					generateManagerWindow(username);
 					System.out.println("Manageer window generated");
 				}
-				
+
 				System.out.println("Login is successful");
 				System.out.println("Logged in as: " + username);
 				myConn.close();
