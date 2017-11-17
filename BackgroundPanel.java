5c30d7ede6b1f621eb13f616d3c53852f2742b70
DavisRomn <davisabr@gmail.com>
2017-11-17 15:19:50 -0700

    Added Comments to BackgroundPanel and Bug

--- BackgroundPanel.java
+++ BackgroundPanel.java
@@ -2,15 +2,18 @@
 import java.awt.*;
 import javax.swing.*;
 
-
+// Background Panel Class
 public class BackgroundPanel extends JPanel {
+    //Image Used for the Background of the Program
     ImageIcon backgroundImage;
 
+    //Constructor for the Class
     public BackgroundPanel (LayoutManager layout, String image) {
 		super(layout);
 		backgroundImage = new ImageIcon(image);
 	}
 
+    // Paints the Image onto the back panel
     protected void paintComponent (Graphics g) {
 		super.paintComponent(g);
 		g.drawImage(backgroundImage.getImage(), 0, 0, null);
--- Bug.java
+++ Bug.java
@@ -1,15 +1,18 @@
 import java.util.Date;
 
+//Bug Class
 public class Bug {
-	private String name;
-	private String product;
+    // Local String Variables:
+	private String name, product, details, assignedDev;
+    // Local Date Variable, stores time created:
 	private Date created;
+    // Local Boolean Variable, shows if Bug is approved or not
 	private Boolean approved;
-	private String details;
+    // Local Int Variable, shows status of the Bug
 	private int status;
-	private String assignedDev;
-	
-	public Bug(String name, String fromProduct, Date created, Boolean approved, 
+
+    // Bug Constructor
+	public Bug(String name, String fromProduct, Date created, Boolean approved,
 			   String details, int status,String assignedDev) {
 		this.name = name;
 		this.product = fromProduct;
@@ -17,61 +20,77 @@ public class Bug {
 		this.approved = approved;
 		this.details = details;
 		this.status = status;
-		this.assignedDev = assignedDev;	
+		this.assignedDev = assignedDev;
 	}
-	
+
+    // Function used to set the name of a Bug
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+    // Function that returns the current name of the Bug
 	public String getName() {
 		return name;
 	}
+
+    // Function that sets the product the Bug is from
 	public void setProduct(String fromProduct) {
 		this.product = fromProduct;
 	}
-	
+
+    // Returns the product with which the bug is from
 	public String getProduct() {
 		return product;
 	}
-	
+
+    // Sets the date for when the Bug was created
 	public void setCreated(Date created) {
 		this.created = created;
 	}
-	
+
+    // Returns the Date the Bug was Created
 	public Date getCreated() {
 		return created;
 	}
-	
+
+    // Sets the Approved status of the Bug
 	public void setApproved(Boolean approved) {
 		this.approved = approved;
 	}
-	
+
+    // Returns whether the Bug has been approved or not
 	public Boolean getApproved() {
 		return approved;
 	}
-	
+
+    // Sets the Details of the Bug
 	public void setDetails(String details) {
 		this.details = details;
 	}
-	
+
+    // Returns the Details of the Bug
 	public String getDetails() {
 		return details;
 	}
-	
+
+    // Sets the Status of the Bug
 	public void setStatus(int status) {
 		this.status = status;
 	}
-	
+
+    // Returns the Status of the Bug
 	public int getStatus() {
 		return status;
 	}
+
+    // Sets the Assigned Dev for the Bug
 	public void setAssignedDev(String assignedDev) {
 		this.assignedDev = assignedDev;
 	}
-	
+
+    // Returns the Developed for which the Bug is Assigned To
 	public String getAssignedDev() {
 		return assignedDev;
 	}
-	
+
 }
